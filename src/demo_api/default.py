3130936da4c9228a8d2293b39877dcc9f4410f3a
Bryan Wyatt <brwyatt@gmail.com>
2019-11-24 11:49:42 -0800

    Some simple implementation to test the library

--- src/demo_api/default.py
+++ src/demo_api/default.py
@@ -1,7 +1,16 @@
+import json
+
 from ophiuchus.framework import Handler
 from ophiuchus.framework import route
 
 
 @route("/")
 class Default(Handler):
-    pass
+    def GET(self, event, context):
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': json.dumps({"message": "yup, hi!"})
+        }
--- src/demo_web/default.py
+++ src/demo_web/default.py
@@ -1,7 +1,35 @@
+import json
+
 from ophiuchus.framework import Handler
 from ophiuchus.framework import route
 
 
 @route("/")
+@route("/{test}")
 class Default(Handler):
-    pass
+    def GET(self, event, context):
+        return self._all(event, context)
+
+    def POST(self, event, context):
+        return self._all(event, context)
+
+    def PUT(self, event, context):
+        return self._all(event, context)
+
+    def DELETE(self, event, context):
+        return self._all(event, context)
+
+    def _all(self, event, context):
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/html',
+            },
+            'body': "\n".join([
+                "<html><head><title>EVENT DETAILS</title><body>",
+                "<h1>EVENT DETAILS</h1><pre>",
+                f"{json.dumps(event, indent=4, sort_keys=True)}",
+                "</pre></body></html>",
+                "",
+            ]),
+        }
